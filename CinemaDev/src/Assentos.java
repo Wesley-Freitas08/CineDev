f769151cf49525e40d23ac7a36e11dcbbd69868a
Wesley-Freitas08 <wetyfreitas@hotmail.com>
2025-09-22 15:16:18 -0300

    feat: adiciona classe Assento com controle de status
    Armazenar a posição do assento (fileira e número).
    
    Controlar o status do assento (L para livre, X para ocupado).
    
    Permitir ocupar e liberar o assento através de métodos específicos.
    
    Verificar se um assento está livre.
    
    Fornecer acesso ao status atual do assento.
    
    Essa classe serve como base para a gestão da sala de cinema, sendo utilizada pela classe SalaDeCinema para representar cada posição disponível.

---
+++ CinemaDev/src/Assentos.java
@@ -0,0 +1,35 @@
+public class Assentos {
+ 
+ //Atributos   
+ private int fileira;
+ private int numero;
+ private char status; //Para reprensentar 'L' ou 'x'
+    
+  //Métodos   
+   
+ public Assentos(int fileira, int numero){
+     this.fileira = fileira;
+     this.numero = numero;
+     this.status = 'L'; // inicializa o assento livre já.
+ }
+ 
+  // Ocupa o assento, Alterando seu status para 'X'.
+ public void ocupar(){
+     this.status = 'X';
+ }
+ 
+ //Libera o assento, alterando seu status para 'L'.
+ public void liberar() {
+     this.status = 'L';
+ }
+ 
+ //Verifica se o assento está livre.
+ public boolean estaLivre(){
+     return this.status == 'L';
+ }
+
+ //Retorna o status atual do assento.
+ public char getStatus(){
+     return this.status;
+   } 
+}
